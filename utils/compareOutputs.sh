f658122146c808eb7153ae24f40c615df025abef
Nicolas Zachow <nicolas@nicolas.eti.br>
2019-07-06 14:47:22 -0300

    Update test script to handle many sources

--- utils/compareOutputs.sh
+++ utils/compareOutputs.sh
@@ -4,26 +4,29 @@ go build -o gg main.go scan.go
 
 options="a aC aD aI aK aN aO aP aR aS aV"
 queries="grep for test 2 true -42 5.25 -5.25"
+sources=". testdata/*"
 
 err=false
 for o in $options; do
     for q in $queries; do
-        ./gg -cpu=1 -summary=false -log=a $o $q . > ./new
-        gg -cpu=1 -summary=false -log=b $o $q . > ./old
-        CHANGES=$(diff ./new ./old | wc -l)
+        for s in $sources; do
+            ./gg -cpu=1 -summary=false -log=a $o $q $s > ./new
+            gg -cpu=1 -summary=false -log=b $o $q $s > ./old
+            CHANGES=$(diff ./new ./old | wc -l)
 
-        if [ $CHANGES -eq 0 ]; then
-            rm -rf ./new ./old ./a ./b
-        else
-            echo "Outputs don't match for 'gg $o $q .'"
-            diff ./new ./old
-            err=true
-            break
-        fi
+            if [ $CHANGES -eq 0 ]; then
+                rm -rf ./new ./old ./a ./b
+            else
+                echo "Outputs don't match for 'gg $o $q $s'"
+                diff ./new ./old
+                err=true
+                break
+            fi
+            if [ $err = true ]; then
+                break
+            fi
+        done
     done
-    if [ $err = true ]; then
-        break
-    fi
 done
 
 if [ $err = false ]; then
