eb694f01a44b5f110e3970fa5589e95d8e8ca710
Nicolas Zachow <nicolas@nicolas.eti.br>
2019-07-06 00:24:15 -0300

    Fix test script
    we had to force the new flag -summary to false, otherwise the
    outputs would be always different.
    
    For some reason if you don't pass -log flag gg will 'randomly' not
    find any result.
    
    I discovered this because I was running this script several times
    and each time it would fail with a different combination of options
    and queries. This would happen even if I didn't change the code
    between runs.
    
    During the debug session I tried many solutions but nothing was
    effective. Two that I think should be noted are; 1) I tried to add
    a sleep statement between calls to gg but I was still facing the
    same issue. 2) I tried to execute the gg command again (while [gg
    output is null] try again) but this just resulted in an endless
    loop.
    
    The bahaviour of not finding anything happened both in my local
    branch 'addTests' and in 'master'. Sometimes my binary would not
    find any reslts and sometimes binary build from master would not
    find anything.
    
    This behaviour gets even stranger because I could not reproduce it
    while typing the command in an interactive shell session. It only
    happened to me while gg was being called from a script.

--- utils/compareOutputs.sh
+++ utils/compareOutputs.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-rm -rf ./gg
 go build -o gg main.go scan.go
 
 options="a aC aD aI aK aN aO aP aR aS aV"
@@ -9,12 +8,12 @@ queries="grep for test 2 true"
 err=false
 for o in $options; do
     for q in $queries; do
-        ./gg $o $q . > ./new
-        gg $o $q . > ./old
+        ./gg -cpu=1 -summary=false -log=a $o $q . > ./new
+        gg -cpu=1 -summary=false -log=b $o $q . > ./old
         CHANGES=$(diff ./new ./old | wc -l)
 
         if [ $CHANGES -eq 0 ]; then
-            rm -rf ./new ./old
+            rm -rf ./new ./old ./a ./b
         else
             echo "Outputs don't match for 'gg $o $q .'"
             diff ./new ./old
@@ -30,3 +29,4 @@ done
 if [ $err = false ]; then
     echo "Nice, everything is still working!"
 fi
+rm -rf ./gg
